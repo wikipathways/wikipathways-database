dca8ae106b2326e1ee4b92511dac2eac142a97d7
Anders Riutta <git@andersriutta.com>
2022-03-01 17:46:39 -0800

    Update GPML -> frontmatter

--- scripts/create_pathway_frontmatter.py
+++ scripts/create_pathway_frontmatter.py
@@ -1,19 +1,29 @@
 import csv
 from datetime import date
 import frontmatter
+from frontmatter.default_handlers import YAMLHandler
 from io import BytesIO
+from pathlib import Path
 import sys
 
 
-info_fp = sys.argv[1]
-if not info_fp:
-    raise Exception('No info_fp provided')
-# TODO: detect the file that changed; don't hardcode this.
-#info_fp = './wikipathways-database/pathways/WP5/WP5.info'
+info_f = sys.argv[1]
+if not info_f:
+    raise Exception('No info_f provided')
 
-# TODO: is there a better way to create an empty post object?
-post = frontmatter.loads('---\n---')
-with open(info_fp) as f:
+info_fp = Path(info_f)
+wpid = info_fp.stem
+
+
+frontmatter_fp = Path('./wikipathways-database/pathways/' + wpid + '/' + wpid + '.md')
+frontmatter_f = str(frontmatter_fp)
+if frontmatter_fp.exists():
+    post = frontmatter.load(frontmatter_f, handler=YAMLHandler())
+else:
+    # TODO: is there a better way to create an empty post object?
+    post = frontmatter.loads('---\n---')
+
+with open(info_f) as f:
     for line in f:
         key, value = line.strip().split(': ', 1)
 
@@ -36,28 +46,25 @@ with open(info_fp) as f:
         post[key] = value
 
 datanode_labels = set()
-with open('./wikipathways-database/pathways/WP554/WP554-datanodes.tsv') as f:
+with open('./wikipathways-database/pathways/' + wpid + '/' + wpid + '-datanodes.tsv') as f:
     reader = csv.DictReader(f, delimiter="\t", quoting=csv.QUOTE_NONE)
     for line in reader:
         datanode_labels.add(line['Label'])
 
-# TODO: read this in from the old file, if it exists
-post['communities'] = []
-
 # TODO: Tina will add this.
-post['github-authors'] = []
+#post['github-authors'] = []
 
 post['redirect_from'] = [
-    '/index.php/Pathway:' + post['wpid'],
-    '/instance/' + post['wpid'],
-    '/instance/' + post['wpid'] + '_r' + post['revision'],
+    '/index.php/Pathway:' + wpid,
+    '/instance/' + wpid,
+    '/instance/' + wpid + '_r' + post['revision'],
 ]
 
 post['seo'] = 'CreativeWork'
 
 post['schema-jsonld'] = [{
     '@context': 'https://schema.org/',
-    '@id': 'https://wikipathways.github.io/pathways/' + post['wpid'] + '.html',
+    '@id': 'https://wikipathways.github.io/pathways/' + wpid + '.html',
     '@type': 'Dataset',
     'name': post['title'],
     'description': post['description'],
@@ -66,5 +73,5 @@ post['schema-jsonld'] = [{
     'keywords': list(datanode_labels),
 }]
 
-with open('./wikipathways-database/pathways/' + post['wpid'] + '/' + post['wpid'] + '.md', 'wb') as f:
+with open(frontmatter_f, 'wb') as f:
     frontmatter.dump(post, f)
