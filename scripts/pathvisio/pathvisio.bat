fe77ffec25f56b3da8aa7a629b68c37296657ac3
Anders Riutta <git@andersriutta.com>
2022-03-01 16:50:30 -0800

    rm old individual workflows. download pathvisio instead of storing in repo.

--- scripts/pathvisio/pathvisio.bat
+++
@@ -1,3 +0,0 @@
-cd /d %~dp0
-
-java -Xmx1024m -jar -Dfile.encoding=UTF-8 pathvisio.jar "$@"
--- scripts/pathvisio/pathvisio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# change to directory of this script
-cd $(dirname $0)
-
-# Run PathVisio
-java -jar -Dfile.encoding=UTF-8 pathvisio.jar "$@"
