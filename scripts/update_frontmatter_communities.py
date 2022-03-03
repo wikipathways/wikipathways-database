c8bb7179bdc8be63a3c2a7a3a7d6a5357fd6b1b2
Anders Riutta <git@andersriutta.com>
2022-03-02 19:09:29 -0800

    Update communities in frontmatter

--- scripts/update_frontmatter_communities.py
+++ scripts/update_frontmatter_communities.py
@@ -11,23 +11,20 @@ communities_fp = Path('./wikipathways-database/communities/')
 
 communities_by_wpid = dict()
 
-for f in communities_fp.glob('**/*.txt'):
-    community = f.stem
-    for wpid in f.readlines():
-        if not wpid in communities_by_wpid:
-            communities_by_wpid[wpid] = set()
+for p in communities_fp.glob('**/*.txt'):
+    community = p.stem
+    with p.open() as f:
+        for wpid in f.read().splitlines():
+            if not wpid in communities_by_wpid:
+                communities_by_wpid[wpid] = set()
 
-        communities_by_wpid[wpid].add(community)
+            communities_by_wpid[wpid].add(community)
 
-
-for wpid, communities in communities_by_wpid:
+for wpid, communities in communities_by_wpid.items():
     frontmatter_f = './wikipathways-database/pathways/' + wpid + '/' + wpid + '.md'
 
     post = frontmatter.load(frontmatter_f, handler=YAMLHandler())
     post['communities'] = list(communities)
-
-    print(wp)
-    print(communities)
 
     with open(frontmatter_f, 'wb') as f:
         frontmatter.dump(post, f)
