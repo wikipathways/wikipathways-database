49f07411e86aac76871d7dd3a6042f61c7fa9558
Egon Willighagen <egon.willighagen@gmail.com>
2023-05-06 20:31:46 +0200

    Also updated the record number

--- scripts/local-run/on-gpml-change_local.sh
+++ scripts/local-run/on-gpml-change_local.sh
@@ -200,7 +200,7 @@ done
 echo "6. ACTION: homologyConversion"
 # NOTE: requires Java 8 
 if [ ! -e ./Hs_Derby_Ensembl_108.bridge ]; then
-    wget -O Hs_Derby_Ensembl_108.bridge "https://zenodo.org/record/6367091/files/Hs_Derby_Ensembl_108.bridge?download=1"
+    wget -O Hs_Derby_Ensembl_108.bridge "https://zenodo.org/record/7781913/files/Hs_Derby_Ensembl_108.bridge?download=1"
 fi
 
 for f in ${changed_gpmls[@]}; do
