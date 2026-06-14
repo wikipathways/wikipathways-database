5e20f0f8d026e098db8c51bff117aa4b74fc3ea3
Egon Willighagen <egon.willighagen@gmail.com>
2026-06-14 08:53:46 +0200

    Updated for python-frontmatter 1.2.0

--- scripts/update_frontmatter_ndex.py
+++ scripts/update_frontmatter_ndex.py
@@ -20,16 +20,15 @@ for p in repo_dir.joinpath('pathways/').glob('WP*/WP*.md'):
     print(wpid)
 
     post = frontmatter.load(str(p), handler=YAMLHandler())
-    
+
     old_ndex = post.get('ndex', str())
     print(old_ndex)
     new_ndex = ndex_by_wpid.get(wpid)
-    
+
     if new_ndex:
         print(f"updating {wpid} ndex from {old_ndex} to {new_ndex}")
         post['ndex'] = new_ndex
-        with Path(p).open('wb') as f:
-            frontmatter.dump(post, f)      
+        frontmatter.dump(post, Path(p))
 
 # wpids specified in ndex_lookup.csv without corresponding wpids in ./pathways
 non_existent_wpids = set(ndex_by_wpid.keys()) - wpids
